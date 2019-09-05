f3a58c5da30d0489136ac873c4312edea135f8b8
EXComm19 <excomm19@icloud.com>
2019-09-05 23:12:35 +1000

    Finished Version 1

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('select')
+    const reset = document.getElementById('reset')
+    const list = document.getElementById('list')
+    const chosen = document.getElementById('chosen')
+
+    btn.addEventListener('click', () => {
+        let rnd = Math.floor(Math.random() * 15)
+        list.children[rnd].style.background = 'lightgrey'
+        list.children[rnd].style.fontSize = '180%'
+        chosen.textContent = "选中的玩家：" + list.children[rnd].innerHTML
+    })
+
+    reset.addEventListener('click', () => {
+        window.location = window.location
+    })
+})
